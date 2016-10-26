e34d579f45b5c8218882d3fc31749a83693b6a62
gcnbroadband <gcnbroadbandindia@gmail.com>
2016-10-26 17:21:11 +0530

    image sim link

--- app/uploaders/address_proof_uploader.rb
+++ app/uploaders/address_proof_uploader.rb
@@ -13,11 +13,7 @@ class AddressProofUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "/home/gcn/uploaders/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-  def cache_dir
-    "/home/gcn/uploaders/tmp/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- config/deploy.rb
+++ config/deploy.rb
@@ -37,4 +37,4 @@ set :deploy_to, '/home/gcn/gcncrmapp'
 # set :keep_releases, 5
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
